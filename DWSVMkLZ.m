a465ebfd4e845ba427b36edf6b39d733ee7e2ae4
Hui (Sophie) Sun <sunhuisophie2012@gmail.com>
2017-10-20 15:31:38 -0400

    Update DWSVMkLZ.m

--- DWSVMkLZ.m
+++ DWSVMkLZ.m
@@ -1,5 +1,46 @@
 function [w, betaa, outstat] = DWSVMkLZ(x_Train,y_Train, CC)
 %%%%%%%%%%%%%% MDWSVM function. Use CVS package, need to install CVX package first.
+% This subroutine is designed to calculate a linear discriminator
+% into K classes based on the training data x_Train,
+% the classification vector y_Train, and the violation cost penalty CC.
+% It uses the MDWSVD method of H. Sun, B. Craig & L.Zhang
+
+% The user will need to get the SDPT3 optimization package, Version 3.02,
+% available at http://www.math.nus.edu.sg/~mattohkc/sdpt3.html,
+% and install it. We recommend that sep.m be run from the SDPT3
+% directory.
+
+% SDPT3 includes an m-file startup.m, which sets the path,
+% default parameters, and global environment variables.
+% If the user has his/her own startup routine, this may not
+% perform the necessary tasks. Either the instructions
+% in SDPT3's startup.m should be appended, or SDPT3's
+% startup.m be renamed startupSDPT3.m, say, and this called
+% at the appropriate place.
+
+% The subroutine contains various commented-out statements, which
+% can be reinstated by those users wishing to see the time taken in
+% various steps, the numbers of variables and constraints, etc.
+
+% Input:  X, a d x n matrix, whose columns contain the training
+%            data;
+%         n x 1 vector y, with y_j the class of vector x_j; and
+%         scalar penalty, the cost for perturbing each residual.
+
+% Output: W, a d x K matrix, and beta, a K x 1 vector,
+%            such that x is classified in class k if the kth
+%            component of the K-vector W'x + beta is largest.
+%         resid, the values of (w_k'x + beta_k) - (w_j'x + beta_j)
+%            for x a column of X with label k.
+%         totalviolation, the total amount added to the residuals.
+%         dualgap, the duality gap, a measure of the accuracy
+%            of the solutions found.
+%         flag, an indication of the success of the computation:
+%             0, success;
+%            -1, inaccurate solution;
+%            -2, problem infeasible or unbounded.
+
+
 if nargin<2 ;
     error('At least two arguments are needed for mdwsvm.m') ;
 end ;
